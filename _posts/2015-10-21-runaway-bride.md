1f76762d9663a1154a65e79197d8ecccfee17067
Jack Reid <emailjackreid@gmail.com>
2015-10-24 09:12:23 +0100

    v1 dump

---
+++ _posts/2015-10-21-runaway-bride.md
@@ -0,0 +1,14 @@
+---
+layout: post
+title: Runaway Bride
+---
+
+![Shropshire Magazine Cover](http://musebridal.co.uk/images/articles/shropshiremagazine.jpg)
+
+Muse Bridal were pleased to be invited to join the Shropshire Magazine for their bridal issue photo shoot.  We enjoyed a lovely afternoon in the autumn sunshine at the Valley Hotel in Ironbridge along with the Editor and staff for the Magazine, and as it was such a beautiful day, they decided to take advantage of our glorious landmark down the road and have a few shots by the Ironbridge. They insisted we went in separate cars as they worried about the new ivory dress in what was claimed to be a messy car.
+
+We caused a bit of a stir when I pulled up by the bridge to let the 'bride' hop out of my little red car.  No groom, no guests, just a young lady in a bridal gown, all alone, running across the road towards the bridge!  I had to giggle.  The cars stopped, the pedestrians stood and stared and heads turned in the teashop.  All of them no doubt thinking they were witnessing a 'Runaway Bride'!
+
+A selection of the wonderful photographs can be seen in the October issue of the Shropshire Magazine and we are particularly proud of our 'cover girl' Hannah looking 'gorgeous' on the front page!
+
+Thank you to the Shropshire Magazine for a lovely experience and The Valley Hotel for a great setting.
--- gulpfile.js
+++ gulpfile.js
@@ -20,29 +20,38 @@ var gulp = require('gulp'),
 
 // SASS
 gulp.task('sass', function() {
-    gulp.src('_sass/style.scss')
-        .pipe(sass({style: 'compressed'}))
-        .pipe(autoprefixer())
-        .pipe(gulp.dest('css/'))
+  gulp.src('_sass/style.scss')
+    .pipe(sass({style: 'compressed'}))
+    .pipe(autoprefixer())
+    .pipe(gulp.dest('css/'))
 });
 
 // JS
 gulp.task('js', function () {
-    gulp.src(['_js/app.js'])
-        .pipe(babel())
-        .pipe(uglify())
-        .pipe(rename('build.js'))
-        .pipe(gulp.dest('js/'))
+  gulp.src(['_js/app.js'])
+    .pipe(uglify())
+    .pipe(rename('build.js'))
+    .pipe(gulp.dest('js/'))
 });
 
 // Jekyll Task
 gulp.task('jekyll', function (gulpCallBack){
-    var spawn = require('child_process').spawn;
-    var jekyll = spawn('jekyll', ['build'], {stdio: 'inherit'});
+  var spawn = require('child_process').spawn;
+  var jekyll = spawn('jekyll', ['build'], {stdio: 'inherit'});
 
-    jekyll.on('exit', function(code) {
-        gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
-    });
+  jekyll.on('exit', function(code) {
+    gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
+  });
+});
+
+// Jekyll Serve Task
+gulp.task('serve', function (gulpCallBack){
+  var spawn = require('child_process').spawn;
+  var jekyll = spawn('jekyll', ['serve'], {stdio: 'inherit'});
+
+  jekyll.on('exit', function(code) {
+    gulpCallBack(code === 0 ? null : 'ERROR: Jekyll process exited with code: '+code);
+  });
 });
 
 // Deploy task
@@ -51,14 +60,21 @@ gulp.task('deploy', function(){
     .pipe(sftp({
       host: "159.203.76.23",
       user: "root",
-      remotePath: "/var/www/jackwreid.uk/public_html/",
+      remotePath: "/var/www/musebridal.co.uk/public_html/",
       key: {location: "~/.ssh/id_rsa", passphrase: "Joe09051989"}
     }));
 });
 
 // Default task
 gulp.task('default', ['sass', 'js', 'jekyll'], function (event) {
-    gulp.watch("_sass/**", ['sass', 'jekyll']);
-    gulp.watch(['*.html', '*/*.html', '*/*.md'], ['jekyll']);
-    gulp.watch("js/*.js", ['js', 'jekyll']);
+  gulp.watch("_sass/**", ['sass', 'jekyll']);
+  gulp.watch(['*.html', '*/*.html', '*/*.md'], ['jekyll']);
+  gulp.watch("js/*.js", ['js', 'jekyll']);
+});
+
+// Local build task
+gulp.task('local', ['sass', 'js', 'jekyll'], function (event) {
+  gulp.watch("_sass/**", ['sass', 'jekyll']);
+  gulp.watch("*.html", ['jekyll']);
+  gulp.watch("js/*.js", ['js', 'jekyll']);
 });
